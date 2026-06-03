a6632a6ecc3a20ceb6d4f132081bb3eadd37d1a7
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-02 20:39:40 -0500

    Export SQLite replay cache

--- triadic_controls/crypto/__init__.py
+++ triadic_controls/crypto/__init__.py
@@ -1,12 +1,14 @@
 """Cryptographic verification primitives for triadic-controls."""
 
 from .replay import InMemoryReplayCache, ReplayCacheProtocol, generate_replay_key
+from .sqlite_replay import SQLiteReplayCache
 from .verifier import CryptoVerifier, VerificationResult
 
 __all__ = [
     "CryptoVerifier",
     "InMemoryReplayCache",
     "ReplayCacheProtocol",
+    "SQLiteReplayCache",
     "VerificationResult",
     "generate_replay_key",
 ]
