c90d6e657641002550eef114b7936b07a49b3301
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-03 17:50:50 -0500

    Export evidence registry APIs

--- cathedral_core/witness/__init__.py
+++ cathedral_core/witness/__init__.py
@@ -1,5 +1,7 @@
 """Witness evidence, receipt, and governance-debt layer."""
 
+from .evidence_promotion import promote_with_basis_check
+from .evidence_registry import EvidenceRecord, EvidenceRegistry, EvidenceRegistryError
 from .governance_debt import DebtType, GovernanceDebt
 from .qgs import QGSLevel, evidence_sufficient
 from .receipt import ProofBundle
@@ -7,7 +9,11 @@ from .receipt import ProofBundle
 __all__ = [
     "DebtType",
     "GovernanceDebt",
+    "EvidenceRecord",
+    "EvidenceRegistry",
+    "EvidenceRegistryError",
     "QGSLevel",
     "evidence_sufficient",
     "ProofBundle",
+    "promote_with_basis_check",
 ]
