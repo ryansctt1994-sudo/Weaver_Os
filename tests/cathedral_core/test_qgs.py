96d833c868e3ed3e7f1937764d7d38dbfdee660b
ryansctt1994-sudo <ryan.sctt1994@gmail.com>
2026-06-03 23:33:31 -0500

    Test canonical audited QGS alias

--- tests/cathedral_core/test_qgs.py
+++ tests/cathedral_core/test_qgs.py
@@ -6,7 +6,11 @@ def test_qgs_levels_are_ordered():
     assert QGSLevel.E1_SPEC < QGSLevel.E2_IMPLEMENTED
     assert QGSLevel.E2_IMPLEMENTED < QGSLevel.E3_RECEIPTED
     assert QGSLevel.E3_RECEIPTED < QGSLevel.E4_REPLICATED
-    assert QGSLevel.E4_REPLICATED < QGSLevel.E5_PRODUCTION_BOUND
+    assert QGSLevel.E4_REPLICATED < QGSLevel.E5_AUDITED
+
+
+def test_e5_production_bound_alias_preserves_backward_compatibility():
+    assert QGSLevel.E5_PRODUCTION_BOUND is QGSLevel.E5_AUDITED
 
 
 def test_evidence_sufficient_accepts_equal_or_higher_level():
